6b5acb474b2afa8d74c558d18330219ba8a293a1
unknown <Aprendiz@BOGDFPCGMF4103.FormacionSena.RED>
2023-08-22 11:40:09 -0500

    Initial commit

---
+++ app/basicscodelab4/src/main/java/com/sena/basicscodelab4/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.sena.basicscodelab4.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val blue1 = Color(0xFF000000)
+val blue2 = Color(0xFF03A9F4)
+val blue3 = Color(0xFFFFFFFF)
+
+val green1 = Color(0xFFFFFFFF)
+val green2 = Color(0xFF03A9F4)
+val green3 = Color(0xFF000000)
---
+++ app/basicscodelab4/src/main/java/com/sena/basicscodelab4/ui/theme/Theme.kt
@@ -0,0 +1,64 @@
+package com.sena.basicscodelab4.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorScheme = darkColorScheme(
+    surface = blue1,
+    onSurface = blue1,
+    primary = blue2,
+    onPrimary = blue3
+
+
+)
+
+private val LightColorScheme = lightColorScheme(
+    surface = green1,
+    onSurface = green1,
+    primary = green2,
+    onPrimary = green3
+)
+
+@Composable
+fun BasicsCodeLab4Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
+
